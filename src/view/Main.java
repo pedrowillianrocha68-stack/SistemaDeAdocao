8a661ba9564d6648201ff6a1e95f8c4ecc31182f
pedrowillian <pedrowillianrocha68@gmail.com>
2025-11-16 13:38:51 -0300

    commit inicial

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 public class Main {
 	public static void main (String[] args) {
 		Gson g = new Gson();
-		System.out.println("GSON FUNCIONANDO");
+		System.out.println("SON FUNCIONANDO");
 	}
 
 }
