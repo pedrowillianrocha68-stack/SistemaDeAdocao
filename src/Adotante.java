431b46ca6a8b41298e94b3bbc890ae0d7c82bcab
davisimoes1 <nunesdavi285@gmail.com>
2025-11-19 22:02:45 -0300

    commit adotante

--- src/Adotante.java
+++ src/Adotante.java
@@ -1,4 +1,25 @@
-
 public class Adotante {
-
+   private String nome; 
+   private int idade; 
+   
+   public Adotante(String nome) { 
+	   this.nome = nome;
+	  
+   }
 }
+     public Adotante(String nome int idade) { 
+    	 this.nome  nome; 
+    	 this.idade  idade; 
+     }
+      public String getNome() { 
+    	  return nome;
+      }
+      public void setNome(String nome) { 
+    	  this.nome = nome;
+      }
+      public getIdade() { 
+    	  return idade; 
+      }
+      public void setIdade(int idade) { 
+    	  this.idade = idade;
+      }
