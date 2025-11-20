6b128deb5c1b983131b428572f4fa6de6ac77e7f
pedrowillian <pedrowillianrocha68@gmail.com>
2025-11-20 00:13:45 -0300

    commit animal

--- src/model/Adotante.java
+++ src/model/Adotante.java
@@ -1,3 +1,4 @@
+package model;
 public class Adotante {
    private String nome; 
    private int idade; 
--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,8 +1,9 @@
 package model; 
+
 public  abstract class Animal {
    private String nome;
    private int idade;
- }
+ 
 public Animal(String nome,int idade) { 
 	this.nome = nome; 
 	this.idade = idade;
