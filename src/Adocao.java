33ba3a88ccd08dcfda5ff519549aee5393021b94
davisimoes1 <nunesdavi285@gmail.com>
2025-11-22 00:00:17 -0300

    commit adocao

---
+++ src/Adocao.java
@@ -0,0 +1,19 @@
+package model;
+
+import model.Adotante;
+import model.Animal;
+
+public class Adocao {
+  private Adotante adotante;
+  private Animal animal;
+}
+public Adocao(adotante adotante ,animal animal) {
+	this.adotante = adotante;
+	this.animal = animal;
+	this.animal.setAdocao(true);
+}
+@Override
+public String toString() { 
+	return " Adoção -> " + Adotante.getNome() + " Adotou! " + Animal.getNome();
+}
+}   
